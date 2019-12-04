5ece2b8fa051514ade4f0f1547bb9e95d650e986
Gusmii <agusmanop@gmail.com>
2019-12-04 13:02:00 +0100

    cambios en equipos

---
+++ view/js/equipos.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $.ajax({
+        type: 'GET',
+        url: '../controller/cEquipos.php',
+        dataType: 'json',
+        success: function (data) {
+            
+            console.log(data);
+        
+
+});
