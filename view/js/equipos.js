588409f7f77af9e6fe807fde91a691704c1a8cea
Gusmii <agusmanop@gmail.com>
2019-12-09 14:00:20 +0100

    cambio arreglado

--- view/js/equipos.js
+++ view/js/equipos.js
@@ -1,34 +1,38 @@
-$( document ).ready(function () {
-
+$(document).ready(function () {
     $.ajax({
         type: 'GET',
         url: '../controller/cEquipos.php',
         dataType: 'json',
-        success: function( result ) {
+        success: function (result) {
 
             console.log(result);
-	             
-	                $(".equipos>table").empty();
-	                var newRow="";
-	                
-                    newRow += '<tr><th>ID EQUIPO</th><th>NOMBRE</th><tr>'
-                    
-	                $.each(result,function(i,equipo) {
-	    
-                        newRow += "<tr><th>"+equipo.idEquipo+"</th>"+"<th data-id="+equipo.idEquipo+"><a id='nombreEquipo'>"+equipo.nombre+"<a></th></tr>";                                    
-                    });
-	                
-	                    $(".equipos>table").append(newRow);
-	    		
+
+            $(".equipos>table").empty();
+            var newRow = "";
+
+            newRow += '<tr><th>ID EQUIPO</th><th>NOMBRE</th><tr>'
+
+            $.each(result, function (i, equipo) {
+
+                newRow += "<tr><th>" + equipo.idEquipo + "</th>" + "<th data-id=" + equipo.idEquipo + "><a class='nombreEquipo'>" + equipo.nombre + "<a></th></tr>";
             
+            });
+
+            $(".equipos>table").append(newRow);
+
+
+            $(".nombreEquipo").click(function () {
+
+                alert("hola");
+
+            });
+
         }
 
     });
 
-});
 
-$("#nombreEquipo").click(function(){
-
-    alert("hola");
+    
 
 });
+
