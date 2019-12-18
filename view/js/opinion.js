7296fa9fb619fc733de31d4685f4b34e9d70c546
CarlosIsla99 <isla.carlos.99@gmail.com>
2019-12-18 13:56:10 +0100

    user star rating

---
+++ view/js/opinion.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+    $.ajax({
+        type: 'GET',
+        url: '../controller/cOpiniones.php',
+        dataType: 'json',
+        success: function (result) {
+
+            console.log(result);
+
+            $(".opiniones").empty();
+            var newRow = "";
+
+            $.each(result, function (i, opiniones) {
+ 
+                newRow += '<div class="card m-3 text-dark">'
+                newRow += '<div class="card-header">'+opiniones.fecha+'</div>'
+                newRow += '<div class="card-body">'
+                newRow += '<p class="card-text">'+opiniones.texto+'</p>'
+                newRow += '<p></p>'
+                newRow += '<span id="star1'+i+'" class="fa fa-star"></span>'
+                newRow += '<span id="star2'+i+'" class="fa fa-star"></span>'
+                newRow += '<span id="star3'+i+'" class="fa fa-star"></span>'
+                newRow += '<span id="star4'+i+'" class="fa fa-star"></span>'
+                newRow += '<span id="star5'+i+'" class="fa fa-star"></span>'
+                newRow += '</div></div>'
+
+ 
+            });
+
+            $(".opiniones").append(newRow);
+            
+                for(var x=0; x<=result.length; x++){
+
+                    var cant = Math.floor((Math.random() * 5) + 1);
+
+                    for(var z=1; z<=cant; z++){
+                        $( '#star'+z+x ).addClass( 'checked' );
+                    }
+                }
+        
+
+        }
+
+    });
+
+
+    
+
+});
+
+
