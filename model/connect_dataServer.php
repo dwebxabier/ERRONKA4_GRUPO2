231e541abd2faeaa9a5c57d61f52265f07ea41d1
CarlosIsla99 <isla.carlos.99@gmail.com>
2019-12-19 17:40:35 +0100

    correcion de connect

--- model/connect_dataServer.php
+++ model/connect_dataServer.php
@@ -4,6 +4,6 @@ class connect_data
 {
     public $host='localhost';
     public $userbbdd='administrador';
-    public $passbbdd='albacete123';
+    public $passbbdd='AlbacetePetanaca1!';
     public $ddbbname='erronka4grupo2';
 }
