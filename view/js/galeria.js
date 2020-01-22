788d53c035d5f72f1a84c8ed914051bc44e2ff65
Gusmii <agusmanop@gmail.com>
2020-01-22 14:31:48 +0100

    La galeria de equipos hecha, solo queda decorar

--- view/js/galeria.js
+++ view/js/galeria.js
@@ -1,5 +1,6 @@
 var idUsuario;
 var idCategoria;
+var nombreCategoria;
 
 $(document).ready(function () {
     // COMPROBACION DE SI LA SESSION ESTA INICIADA 
@@ -26,8 +27,22 @@ $(document).ready(function () {
           userCheck(data);
           console.log(data);
           
-          idUsuario= data.idUsuario;
+          idUsuario = data.idUsuario;
+          idCategoria = data.idCategoria;
+
+          if (idCategoria == 3){
+            nombreCategoria="Senior";
+          }
+
+          if (idCategoria == 2){
+            nombreCategoria="Infantil";
+          }
+
+          if (idCategoria == 1){
+            nombreCategoria="Alevin";
+          }
           
+          alert(nombreCategoria);
 
 
           alert(idUsuario);
@@ -53,18 +68,23 @@ $(document).ready(function () {
   
               console.log(result);
   
-              $(".jugadores-mvp").empty();
+              $(".Galeria-Publi").empty();
               var newRow = "";
   
               $.each(result, function (i, jugador) {
   
                 newRow += '<div class="card m-3 carousel" style="width: 18rem;">'
                 newRow += '<img src="'+jugador.fotoEquipo+'">'
+                newRow += '<div>'
+                newRow += '<p'+nombreCategoria+'"p>'
                 newRow += '</div>'
-  
+                newRow += '</div>'
+                
+           
             });
-  
-              $(".jugadores-mvp").append(newRow);
+              
+              fotosPrivadas();
+              $(".Galeria-Publi").append(newRow);
                   
   
               
@@ -73,7 +93,41 @@ $(document).ready(function () {
       });
   
   
+      function fotosPrivadas(){
+
+        $.ajax({
+          type: 'GET',
+          data:{'idCategoria': idCategoria},
+          url: '../controller/cEquiposGaleriaPriv.php',
+          dataType: 'json',
+          success: function (result) {
   
+              console.log(result);
+
+
+            if(idUsuario !== undefined | null){
+
+              var newRow = "";
+  
+              $.each(result, function (i, jugador) {
+  
+
+                newRow += '<div class="card m-3 carousel" style="width: 18rem;">'
+                newRow += '<img src="'+jugador.fotoEquipo+'">'
+                newRow += '<div>'
+                newRow += '<p'+nombreCategoria+'"p>'
+                newRow += '</div>'
+                newRow += '</div>'
+            });
+  
+              $(".Galeria-Priv").append(newRow);
+            }
+          }
+  
+      });
+
+
+      }
   
   
       
